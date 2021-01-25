6b133fbfd16d3bb70daa04b7758feac941c757fa
Adib Naghashian <adib.naghashian@rwth-aachen.de>
2021-01-25 23:35:38 +0100

    removing line /w led.pixels

--- python/visualization.py
+++ python/visualization.py
@@ -155,7 +155,6 @@ def microphone_update(audio_samples):
     vol = np.max(np.abs(y_data))
     if vol < config.MIN_VOLUME_THRESHOLD:
         print('No audio input. Volume below threshold. Volume:', vol)
-        led.pixels = np.tile(0, (3, config.N_PIXELS))
     else:
         # Transform audio input into the frequency domain
         N = len(y_data)
