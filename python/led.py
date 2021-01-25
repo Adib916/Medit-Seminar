445bf640ca11bd38c2b7ee1b500cb61339250774
Adib Naghashian <adib.naghashian@rwth-aachen.de>
2021-01-25 23:13:25 +0100

    More removing LED code

--- python/led.py
+++ python/led.py
@@ -10,13 +10,6 @@ import config
 strip = Adafruit_NeoPixel(config.N_PIXELS, config.LED_PIN,
                           config.LED_FREQ_HZ, config.LED_DMA,
                           config.LED_INVERT, config.BRIGHTNESS)
-strip.begin()
-
-_gamma = np.load(config.GAMMA_TABLE_PATH)
-"""Gamma lookup table used for nonlinear brightness correction"""
-
-_prev_pixels = np.tile(253, (3, config.N_PIXELS))
-"""Pixel values that were most recently displayed on the LED strip"""
 
 pixels = np.tile(1, (3, config.N_PIXELS))
 """Pixel values for the LED strip"""
