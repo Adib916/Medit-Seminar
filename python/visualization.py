1c412ef35ba8a98c12b654e2653e01d3e37c206b
Adib Naghashian <adib.naghashian@rwth-aachen.de>
2021-01-28 23:37:42 +0100

    Removed comments from code and stored it in textfile

--- python/visualization.py
+++ python/visualization.py
@@ -23,22 +23,8 @@ _fps = dsp.ExpFilter(val=config.FPS, alpha_decay=0.2, alpha_rise=0.2)
 
 
 def frames_per_second():
-    """Return the estimated frames per second
+    """Return the estimated frames per second"""
 
-    Returns the current estimate for frames-per-second (FPS).
-    FPS is estimated by measured the amount of time that has elapsed since
-    this function was previously called. The FPS estimate is low-pass filtered
-    to reduce noise.
-
-    This function is intended to be called one time for every iteration of
-    the program's main loop.
-
-    Returns
-    -------
-    fps : float
-        Estimated frames-per-second. This value is low-pass filtered
-        to reduce noise.
-    """
     global _time_prev, _fps
     time_now = time.time() * 1000.0
     dt = time_now - _time_prev
